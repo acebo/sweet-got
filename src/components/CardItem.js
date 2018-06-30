f8a64322cc601a4e0c22935efd963e1566dea499
Holly Aksornsin <rujira.a@appman.co.th>
2018-06-30 13:44:20 +0700

    [CardItem] Use shouldComponentDidUpdate.

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ProfileImage from './ProfileImage'
 import ProfileDetail from './ProfileDetail'
 
-const CardItem = ({ style, isOpenMenuCard, onToggleCard, onDeleteItem, ...item }) => (
-  <li style={style}
-      className={isOpenMenuCard ? 'open-menu' : undefined}
-      onClick={() => onToggleCard(item.Id)}>
-    <div className={"card-menu"} onClick={(e) => {
-      onDeleteItem(item.Id)
-      e.stopPropagation()}}
-    >
-      <div className={"delete-button"}>X Delete</div>
-    </div>
-    <div className={"card-item"}>
-      <ProfileImage src={item.ProfileImage} name={item.Name} />
-      <ProfileDetail name={item.Name} aliases={item.Aliases.join(', ')} />
-    </div>
-  </li>
-)
+class CardItem extends Component {
+  shouldComponentUpdate(nextProps) {
+    return this.props.isOpenMenuCard !== nextProps.isOpenMenuCard
+  }
+
+  render() {
+    const { style, isOpenMenuCard, onToggleCard, onDeleteItem, ...item } = this.props
+    return (
+      <li style={style}
+          className={isOpenMenuCard ? 'open-menu' : undefined}
+          onClick={() => onToggleCard(item.Id)}>
+        <div className={"card-menu"} onClick={(e) => {
+          onDeleteItem(item.Id)
+          e.stopPropagation()}}
+        >
+          <div className={"delete-button"}>X Delete</div>
+        </div>
+        <div className={"card-item"}>
+          <ProfileImage src={item.ProfileImage} name={item.Name} />
+          <ProfileDetail name={item.Name} aliases={item.Aliases.join(', ')} />
+        </div>
+      </li>
+    )
+  }
+}
 
 export default CardItem
