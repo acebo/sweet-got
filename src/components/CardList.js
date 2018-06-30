89e18c9cd3e4c066446b99219bcb989815adb365
Holly Aksornsin <rujira.a@appman.co.th>
2018-06-30 14:32:48 +0700

    [CardList] Use react-virtualized to display a large list.

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,18 +1,36 @@
 import React from 'react'
+import { List } from 'react-virtualized'
+import { memoizeWith, identity } from 'ramda'
 import CardItem from './CardItem'
 
-const CardList = ({ items, onToggleCard, onDeleteItem, selectedCardId }) => (
+// memoized clientWidth and clientHeight
+const getClientWidth = memoizeWith(identity, () => document.getElementById('root').clientWidth + 400)
+const getClientHeight = memoizeWith(identity, () => document.getElementById('root').clientHeight)
+
+const getCardItem = ({ onToggleCard, onDeleteItem, selectedCardId, style, ...item }) => (
+  <CardItem {...item}
+    key={`${item.Id}-${item.Name}`}
+    style={style}
+    onToggleCard={onToggleCard}
+    onDeleteItem={onDeleteItem}
+    isOpenMenuCard={selectedCardId === item.Id}
+  />
+)
+
+const CardList = ({ items, ...props }) => (
   <div id={"card-list-container"}>
     <ul id={"card-list"}>
-      {
-        items.map(item => <CardItem
-          {...item}
-          key={`${item.Id}-${item.Name}`}
-          onToggleCard={onToggleCard}
-          onDeleteItem={onDeleteItem}
-          isOpenMenuCard={selectedCardId === item.Id}
-        />)
-      }
+      <List
+        width={getClientWidth()}
+        height={getClientHeight()}
+        rowHeight={232}
+        rowCount={items.length}
+        rowRenderer={rowProps => getCardItem({
+          ...props,
+          ...items[rowProps.index],
+          style: rowProps.style
+        })}
+      />
     </ul>
   </div>
 )
